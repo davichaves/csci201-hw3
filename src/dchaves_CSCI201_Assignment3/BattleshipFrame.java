2324bfa95841922300bf5d88c070f5e13c29ff37
Davi Chaves <dchaves@usc.edu>
2015-02-28 11:25:55 -0800

    random

--- src/dchaves_CSCI201_Assignment3/BattleshipFrame.java
+++ src/dchaves_CSCI201_Assignment3/BattleshipFrame.java
@@ -2,16 +2,19 @@ package dchaves_CSCI201_Assignment3;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 @SuppressWarnings("serial")
 public class BattleshipFrame extends JFrame{
@@ -22,7 +25,33 @@ public class BattleshipFrame extends JFrame{
 
 		JLabel pLabel = new JLabel("Player", SwingConstants.CENTER);
 		JLabel cLabel = new JLabel("Computer", SwingConstants.CENTER);
-		JLabel log = new JLabel("Log:");
+		JLabel log = new JLabel("Log: Edit mode");
+		JLabel file = new JLabel("File:");
+		log.setFont(new Font("Arial", Font.PLAIN, 24));
+		
+		JPanel chooseFile = new JPanel();
+		chooseFile.setLayout(new GridLayout(1,2));
+		
+		JButton selectFile = new JButton("Select File:");
+		selectFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				JFileChooser chooser = new JFileChooser();
+			    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+			        ".battle files", "battle");
+			    chooser.setFileFilter(filter);
+			    int returnVal = chooser.showOpenDialog(getParent());
+			    if(returnVal == JFileChooser.APPROVE_OPTION) {
+			       System.out.println("You chose to open this file: " +
+			            chooser.getSelectedFile().getName());
+			    }
+			}
+		});
+		
+		JButton start = new JButton("START");
+		
+		chooseFile.add(selectFile);
+		chooseFile.add(file);
+		chooseFile.add(start);
 				
 		bsgp.setBorder(BorderFactory.createLineBorder(Color.black));
 		bsgc.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -35,6 +64,7 @@ public class BattleshipFrame extends JFrame{
 		JPanel computer = new JPanel(new BorderLayout());
 		computer.add(bsgc,BorderLayout.CENTER);
 		computer.add(cLabel, BorderLayout.PAGE_START);
+		computer.add(chooseFile, BorderLayout.PAGE_END);
 				
 		add(player);
 		add(computer);
